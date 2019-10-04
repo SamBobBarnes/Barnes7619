8513bdbcd3ff7142d644a612041d0dd80158d9b3
SamBobBarnes <samuelrbarnes@outlook.com>
2019-10-04 06:57:28 -0500

    Fixed sidebar functionality

--- www/html/JAVA/SideBar.js
+++ www/html/JAVA/SideBar.js
@@ -1,72 +1,79 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var sidebarhtml = "";
-  var sidebartext = ["Samuel Barnes","Aundrea Barnes"];
-  var sidebarhref = ["samuelbarnes.html", "aundreabarnes.html"];
+class SideBar {
 
-  sidebarcreate(sidebartext, sidebarhref);
-
-
-});
-
-function sidebaritemcreate(text, href, first){
-  var html = "<div class='sidebaritem ComicSans20'><a href='";
-  var fhtml = "<div class='sidebaritem ComicSans20' style='margin-top: 10px;'><a href='"
-  var htmlend = "'>";
-  var end = "</a></div>";
-  fhtml = fhtml + href + htmlend + text + end;
-  html = html + href + htmlend + text + end;
-  if (first == true) {return fhtml;}
-  else {return html;}
-}
-
-function countbuttons(list){
-  var lookup = "<a href";
-  var lookuptxt = "";
-  var num = 0;
-  for (i = 0; i < list.length; i++){
-    if (lookuptxt.length == 7){
-      if (lookup == lookuptxt) num++;
-      lookuptxt = lookuptxt[1] +  lookuptxt[2] + lookuptxt[3] + lookuptxt[4] + lookuptxt[5] + lookuptxt[6] + list[i];
-    } else lookuptxt = lookuptxt + list[i];
-  }
-  return num;
-}
-
-function sidebarcreate(sidebartext, sidebarhref){
-
-  var sidebaritemhtml = "";
-
-  for (i = 0; i < sidebartext.length; i++){
-    if (i == 0) {sidebaritemhtml = sidebaritemhtml + sidebaritemcreate(sidebartext[i],sidebarhref[i],true);}
-    else {sidebaritemhtml = sidebaritemhtml + sidebaritemcreate(sidebartext[i],sidebarhref[i],false);}
+  sidebaritemcreate(text, href, first){
+    var html = "<div class='sidebaritem ComicSans20'><a href='";
+    var fhtml = "<div class='sidebaritem ComicSans20' style='margin-top: 10px;'><a href='"
+    var htmlend = "'>";
+    var end = "</a></div>";
+    fhtml = fhtml + href + htmlend + text + end;
+    html = html + href + htmlend + text + end;
+    if (first == true) {return fhtml;}
+    else {return html;}
   }
 
-  var googlesearch = google();
-  var stuff = sidebaritemhtml;
-  var num = countbuttons(stuff);
-  var height = 50;
-  var height = height + num * 32;
-  var sidebarhtml = googlesearch + stuff;//sidebarstuff;
-  document.getElementById("sidebar").innerHTML = sidebarhtml;
-  document.getElementById("sidebar").style.height = height + "px";
-}
+  countbuttons(list){
+    var lookup = "<a href";
+    var lookuptxt = "";
+    var num = 0;
+    for (var i = 0; i < list.length; i++){
+      if (lookuptxt.length == 7){
+        if (lookup == lookuptxt) num++;
+        lookuptxt = lookuptxt[1] +  lookuptxt[2] + lookuptxt[3] + lookuptxt[4] + lookuptxt[5] + lookuptxt[6] + list[i];
+      } else lookuptxt = lookuptxt + list[i];
+    }
+    return num;
+  }
 
-function google(){
-  var onfocustext1 = "if(this.value==this.defaultValue)this.value=''; this.style.color='black';";
-  var onfocustext2 = "if(this.value=='')this.value=this.defaultValue;";
+  sidebarcreate(sidebartext, sidebarhref){
 
-  var googlesearch =
-    "<div class='google'>" +
-      "<form method='get' action='http://www.google.com/search'>" +
-      "<table border='0' align='center' cellpadding='0'>" +
-      "<tr><td>" +
-        "<input type='text'   name='q' size='25' style='color:#808080;'" +
-        "maxlength='255' value='Google site search'" +
-        "onfocus='" + onfocustext1 + "' onblur='" + onfocustext2 + "'/>" +
-        "<input type='submit' value='Go!' />" +
-        "<input type='hidden' name='sitesearch' /></td></tr>" +
-      "</table>" +
-    "</div>";
+    var sidebaritemhtml = "";
 
-  return googlesearch;
+    for (var i = 0; i < sidebartext.length; i++){
+      if (i == 0) {sidebaritemhtml = sidebaritemhtml + this.sidebaritemcreate(sidebartext[i],sidebarhref[i],true);}
+      else {sidebaritemhtml = sidebaritemhtml + this.sidebaritemcreate(sidebartext[i],sidebarhref[i],false);}
+    }
+
+    var googlesearch = this.google();
+    var stuff = sidebaritemhtml;
+    var num = this.countbuttons(stuff);
+    var height = 50;
+    var height = height + num * 32;
+    var sidebarhtml = googlesearch + stuff;//sidebarstuff;
+    document.getElementById("sidebar").innerHTML = sidebarhtml;
+    document.getElementById("sidebar").style.height = height + "px";
+  }
+
+  google(){
+    var onfocustext1 = "if(this.value==this.defaultValue)this.value=''; this.style.color='black';";
+    var onfocustext2 = "if(this.value=='')this.value=this.defaultValue;";
+
+    var googlesearch =
+      "<div class='google'>" +
+        "<form method='get' action='http://www.google.com/search'>" +
+        "<table border='0' align='center' cellpadding='0'>" +
+        "<tr><td>" +
+          "<input type='text'   name='q' size='25' style='color:#808080;'" +
+          "maxlength='255' value='Google site search'" +
+          "onfocus='" + onfocustext1 + "' onblur='" + onfocustext2 + "'/>" +
+          "<input type='submit' value='Go!' />" +
+          "<input type='hidden' name='sitesearch' /></td></tr>" +
+        "</table>" +
+      "</div>";
+
+    return googlesearch;
+  }
+
+  constructor(text, href){
+    //console.log("sidebar");
+    if (!(text === undefined)) this.sidebartext = text;
+    else this.sidebartext = ["Home","Samuel Barnes","Aundrea Barnes"];
+    if (!(href === undefined)) {this.sidebarhref = href;} //console.log(href);}
+    else {this.sidebarhref = ["index.html","samuelbarnes.html", "aundreabarnes.html"];} //console.log("executed");}
+
+    //document.addEventListener("DOMContentLoaded", function(event) {
+    var sidebarhtml = "";
+    //console.log("dom");
+    this.sidebarcreate(this.sidebartext, this.sidebarhref);
+    //});
+  }
 }
