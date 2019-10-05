3462b2c04b3cbd1187ac10305af6afefb2a25502
SamBobBarnes <samuelrbarnes@outlook.com>
2019-10-05 11:57:31 -0500

    troubleshooting isMobileDevice

--- www/JAVA/index.js
+++ www/JAVA/index.js
@@ -1,5 +1,8 @@
 if(!isMobileDevice) {
   window.location.replace("http://m.barnes7619.com");
+  document.addEventListener("DOMContentLoaded",function(event){
+    document.getElementById("body").innerHTML = "Im a phone";
+  });
 } /*else {
   document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("body").innerHTML = "Not a Phone";
