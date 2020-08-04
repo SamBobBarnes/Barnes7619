679b5ff024ad6588d30f971c79d6b1d8a33f7084
SamBobBarnes <samuelrbarnes@outlook.com>
2020-08-04 16:40:04 -0500

    fixed google search added portfolio link to samuel barnes page

--- www/JAVA/SideBar.js
+++ www/JAVA/SideBar.js
@@ -10,7 +10,6 @@ function google(){
         "<input type='text'   name='q' size='25' style='color:#808080;'" +
         "maxlength='255' value='Google site search'" +
         "onfocus='" + onfocustext1 + "' onblur='" + onfocustext2 + "'/>" +
-        "<input type='submit' value='Go!' style='width:15px; margin-right: auto; margin-left: auto;'/>" +
         "<input type='hidden' name='sitesearch' /></td></tr>" +
       "</table>" +
     "</div>";
@@ -19,36 +18,43 @@ function google(){
 }
 
 function itemSamuelBarnes(first){
-  var html = "<div class='sidebaritem ComicSans20' id='itemSamuelBarnes'>Samuel Barnes</div>";
-  var fhtml = "<div class='sidebaritem ComicSans20' id='itemSamuelBarnes' style='margin-top: 10px;')'>Samuel Barnes</div>";
+  var html = "<div class='sidebaritem PatrickHand20' id='itemSamuelBarnes'>Samuel Barnes</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemSamuelBarnes' style='margin-top: 10px;')'>Samuel Barnes</div>";
   if (first) return fhtml;
   else return html;
 }
 
 function itemSamuelResume(first){
-  var html = "<div class='sidebaritem ComicSans20' id='itemSamuelResume'>Resume</div>";
-  var fhtml = "<div class='sidebaritem ComicSans20' id='itemSamuelResume' style='margin-top: 10px;')'>Resume</div>";
+  var html = "<div class='sidebaritem PatrickHand20' id='itemSamuelResume'>Resume</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemSamuelResume' style='margin-top: 10px;')'>Resume</div>";
+  if (first) return fhtml;
+  else return html;
+}
+
+function itemSamuelPortfolio(first){
+  var html = "<div class='sidebaritem PatrickHand20' id='itemSamuelPortfolio'>Portfolio Links</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemSamuelPortfolio' style='margin-top: 10px;')'>Portfolio Links</div>";
   if (first) return fhtml;
   else return html;
 }
 
 function itemAundreaBarnes(first){
-  var html = "<div class='sidebaritem ComicSans20' id='itemAundreaBarnes'>Aundrea Barnes</div>";
-  var fhtml = "<div class='sidebaritem ComicSans20' id='itemAundreaBarnes' style='margin-top: 10px;')'>Aundrea Barnes</div>";
+  var html = "<div class='sidebaritem PatrickHand20' id='itemAundreaBarnes'>Aundrea Barnes</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemAundreaBarnes' style='margin-top: 10px;')'>Aundrea Barnes</div>";
   if (first) return fhtml;
   else return html;
 }
 
 function itemAundreaResume(first){
-  var html = "<div class='sidebaritem ComicSans20' id='itemAundreaResume'>Resume</div>";
-  var fhtml = "<div class='sidebaritem ComicSans20' id='itemAundreaResume' style='margin-top: 10px;')'>Resume</div>";
+  var html = "<div class='sidebaritem PatrickHand20' id='itemAundreaResume'>Resume</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemAundreaResume' style='margin-top: 10px;')'>Resume</div>";
   if (first) return fhtml;
   else return html;
 }
 
 function itemHome(first){
-  var html = "<div class='sidebaritem ComicSans20' id='itemHome'>Home</div>";
-  var fhtml = "<div class='sidebaritem ComicSans20' id='itemHome' style='margin-top: 10px;')'>Home</div>";
+  var html = "<div class='sidebaritem PatrickHand20' id='itemHome'>Home</div>";
+  var fhtml = "<div class='sidebaritem PatrickHand20' id='itemHome' style='margin-top: 10px;')'>Home</div>";
   if (first) return fhtml;
   else return html;
 }
